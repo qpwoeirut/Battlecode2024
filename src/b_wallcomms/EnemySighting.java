cb0d5f245869c9e1be48382ef88363fea6142b90
qpwoeirut <stanleyzhong8@gmail.com>
2024-01-13 14:58:19 -0800

    Refresh sightings more often

--- src/b_wallcomms/EnemySighting.java
+++ src/b_wallcomms/EnemySighting.java
@@ -3,8 +3,8 @@ package b_wallcomms;
 import battlecode.common.MapLocation;
 
 public class EnemySighting {
-    final static int STALE = 12;
-    final static int NEARBY_DIST = 20;
+    final static int STALE = 6;
+    final static int NEARBY_DIST = 10;
     MapLocation location;
     int lastUpdate;
 
