a04d77027c0d7508959ee4a42405b5a471b8383f
qpwoeirut <stanleyzhong8@gmail.com>
2024-01-15 15:55:30 -0800

    Create tournament_sprint1 submission

---
+++ src/tournament_sprint1/EnemySighting.java
@@ -0,0 +1,36 @@
+package tournament_sprint1;
+
+import battlecode.common.MapLocation;
+
+public class EnemySighting {
+    final static int STALE = 6;
+    final static int NEARBY_DIST = 12;
+    MapLocation location;
+    int lastUpdate;
+
+    public EnemySighting(MapLocation location, int round) {
+        this.location = location;
+        this.lastUpdate = round;
+    }
+
+    public boolean mergeIn(MapLocation location, int round) {
+        if (stale(round)) {
+            this.location = location;
+            this.lastUpdate = round;
+            return true;
+        } else if (near(location)) {
+            this.location = new MapLocation((this.location.x * 2 + location.x) / 3,(this.location.y * 2 + location.y) / 3);
+            this.lastUpdate = round;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean near(MapLocation location) {
+        return this.location.isWithinDistanceSquared(location, NEARBY_DIST);
+    }
+
+    public boolean stale(int round) {
+        return lastUpdate + STALE <= round;
+    }
+}
